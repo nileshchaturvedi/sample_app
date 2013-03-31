5deb6a898e3d7045b36d4de740f1faa78dedfea3
vagrant <vagrant@precise32.(none)>
2013-03-31 23:55:55 +0000

    Add SSL in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+resources :users
 
   root to: 'static_pages#home'
 
@@ -7,6 +7,7 @@ match '/signup'=> 'users#new'
   match '/help' => 'static_pages#help'
   match '/about' => 'static_pages#about'
   match '/contact' => 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(:version => 20130316185706) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
-    t.string   "string"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
